27fe04cf0203ee68cb5034849065e33478bbac0f
Sebastien R <139985553+Sebastien-Ren@users.noreply.github.com>
2025-12-21 23:11:07 +0100

    loaded mtl's boundary and placed on map

--- map_test.py
+++ map_test.py
@@ -6,4 +6,10 @@ import matplotlib
 
 m = folium.Map(location=(45.5019, -73.5674), control_scale=True, zoom_start=15)
 
+mtl_boundaries = osmnx.geocoder.geocode_to_gdf("Montreal", which_result=None)
+
+#print(mtl_boundaries.head())
+
+folium.GeoJson(mtl_boundaries).add_to(m)
+
 m.save("index.html")
