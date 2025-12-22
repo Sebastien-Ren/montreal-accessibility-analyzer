cd87efe739cf7144e5982872e3a7667624db2b1b
Sebastien R <139985553+Sebastien-Ren@users.noreply.github.com>
2025-12-22 21:58:07 +0100

    gathered data for points of grocery stores and added to map

--- map_test.py
+++ map_test.py
@@ -12,4 +12,19 @@ mtl_boundaries = osmnx.geocoder.geocode_to_gdf("Montreal", which_result=None)
 
 folium.GeoJson(mtl_boundaries).add_to(m)
 
+mtl_pts_of_interest = osmnx.features.features_from_place("Montreal", {'shop': ['supermarket','grocery']}, which_result=None)
+
+#filter for only points, no polygons for now
+mtl_pts_of_interest = mtl_pts_of_interest[mtl_pts_of_interest.geometry.type == 'Point']
+
+print(f"Found {len(mtl_pts_of_interest)} stores")
+#print(mtl_pts_of_interest.head())
+
+for index, row in mtl_pts_of_interest.iterrows():
+    y = row.geometry.y
+    x = row.geometry.x
+
+    folium.Marker(location=[y, x]).add_to(m)
+
+
 m.save("index.html")
